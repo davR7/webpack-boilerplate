7ab50b6329db749a01b172804501f1c3be6413cf
Davidson Ratis <davidsonmakarios@gmail.com>
2021-01-31 17:30:03 -0300

    configuring directory for css file (build)

--- wb/props/plugins.js
+++ wb/props/plugins.js
@@ -1,9 +1,13 @@
 const HtmlPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = [
   new HtmlPlugin({
     filename: 'index.html',
     title: 'Webpack Boilerplate',
     template: 'app/templates/home.hbs'
+  }),
+  new MiniCssExtractPlugin({
+    filename: 'css/styles.css'
   })
 ]
