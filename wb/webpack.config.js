a3a5ec25f4067a54e3563a56b2dec017b46312be
Davidson Ratis <davidsonmakarios@gmail.com>
2021-01-31 16:20:47 -0300

    importing devServer, plugins and rules

--- wb/webpack.config.js
+++ wb/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const option = process.env.NODE_ENV !== 'build'
+const devServer = require('./props/devServer')
+const rules = require('./props/rules')
+const plugins = require('./props/plugins')
 
 module.exports = {
   entry: './app/main.js',
@@ -7,5 +10,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js'
-  }
+  },
+  devServer,
+  module: { rules },
+  plugins
 }
